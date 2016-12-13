75757e145a694551d0b0f740cf7b83b202672ced
sam <samuele.reghenzi@eng.it>
2016-12-13 16:14:08 +0100

    version upgrade

---
+++ src/main/resources/archetype-resources/src/test/scala/SetSuite.scala
@@ -0,0 +1,21 @@
+package ${package}
+
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class SetSuite extends FunSuite {
+
+  test("An empty Set should have size 0") {
+    assert(Set.empty.size == 0)
+  }
+
+  test("Invoking head on an empty Set should produce NoSuchElementException") {
+    assertThrows[NoSuchElementException] {
+      Set.empty.head
+    }
+  }
+}
